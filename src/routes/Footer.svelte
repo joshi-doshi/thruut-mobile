4c85821ef727c84392f2b5e4a3a0948d32022b3d
joshi-doshi <62746556+joshi-doshi@users.noreply.github.com>
2025-11-20 19:29:02 +0100

    implement footer

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { MessageCircle, Settings, Plus } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<footer class="sticky bottom-0 flex w-screen items-center justify-center gap-6 p-6">
+	<Button variant="secondary" class="px-8 py-4 text-sm">
+		<MessageCircle fill="1" /> chat
+	</Button>
+
+	<Plus />
+	<Settings />
+</footer>
